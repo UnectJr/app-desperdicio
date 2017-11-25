d89b5e7e10ce54290697f43d2c7b8c6dc1112c2f
Vinicius <vinicius0809@gmail.com>
2017-11-24 23:05:07 -0200

    Firebase atualizado para a conta dos contratantes

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,13 +1,13 @@
 import * as firebase from "firebase"; 
 
 // Initialize Firebase
-    export var firebaseConfig = {
-      apiKey: "AIzaSyBiMgfyZ_izdFKLDl05vftWixxaS6xKIyM",
-      authDomain: "app-agua-utfpr.firebaseapp.com",
-      databaseURL: "https://app-agua-utfpr.firebaseio.com",
-      projectId: "app-agua-utfpr",
-      storageBucket: "app-agua-utfpr.appspot.com",
-      messagingSenderId: "142712018169"
+    export var  firebaseConfig = {
+      apiKey: "AIzaSyBBoXykJeL6R5V9CF-blJmclL8W5iWif6c",
+      authDomain: "projetoextensaoutfpr-80b0e.firebaseapp.com",
+      databaseURL: "https://projetoextensaoutfpr-80b0e.firebaseio.com",
+      projectId: "projetoextensaoutfpr-80b0e",
+      storageBucket: "projetoextensaoutfpr-80b0e.appspot.com",
+      messagingSenderId: "251315204523"
     };
     firebase.initializeApp(firebaseConfig);
 
